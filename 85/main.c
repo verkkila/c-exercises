ea452456d784552438728f8564f4a8a1d2df4c4d
verkkila <valtteri.erkkila@gmail.com>
2016-06-11 14:03:21 +0300

    85: Done

--- 85/main.c
+++ 85/main.c
@@ -3,9 +3,16 @@
 int main(void)
 {
         FILE *namesfile = NULL;
+        char c = 0;
         namesfile = fopen("names.txt", "r");
         if (!namesfile)
                 return 0;
+        while ((c = getc(namesfile)) != EOF) {
+                if (c == ';')
+                        putchar(' ');
+                else
+                        putchar(c);
+        }
         fclose(namesfile);
         return 0;
 }
