0577d14b4a6a55603e826487f1d384537ccfb3cc
Valtteri Erkkilä <valtteri.erkkila@gmail.com>
2016-06-12 16:36:30 +0300

    84: Added comments, changed variable names

--- 84/main.c
+++ 84/main.c
@@ -6,33 +6,44 @@
 
 int parse_expr(const char *expr, int *a, char *e, int *b);
 int eval(const int a, const char e, const int b);
-int scan_next_expr(const char *buf, char *expr);
+int search_highest_expr(const char *buf, char *expr);
 void remove_whitespace(char *str);
 
-char OPS[] = {'*', '/', '+', '-', '^', '^'};
+char OPERATORS[] = {'*', '/', '+', '-', '^', '^'};
 
 int main(int argc, char **argv)
 {
-        char argvbuf[256] = {0};
+        char exprbuf[256] = {0};
         char original_expr[256] = {0};
         char curexpr[32] = {0};
-        int i = 1, res = 0, exprstart;
+        int i = 1, res = 0, exprstart = 0, has_run_once = 0;
         for (i = 1; i < argc; ++i) {
-                strcat(argvbuf, argv[i]);
-                strcpy(original_expr, argvbuf);
+                strcat(exprbuf, argv[i]);
+                strcpy(original_expr, exprbuf);
         }
-        while ((exprstart = scan_next_expr(argvbuf, curexpr)) != -1) {
+        /*Search for the expression with the highest precedence*/
+        while ((exprstart = search_highest_expr(exprbuf, curexpr)) != -1) {
                 int lhs = 0, rhs = 0, nullidx = 0;
                 char op = 0;
+                /*Parse an expression into the left-hand value, operator and the right-hand value*/
                 parse_expr(curexpr, &lhs, &op, &rhs);
+                /*Evaluate the expression*/
                 res = eval(lhs, op, rhs);
-                memset(&argvbuf[exprstart], ' ', strlen(curexpr));
-                nullidx = sprintf(&argvbuf[exprstart], "%i", res);
-                argvbuf[exprstart + nullidx] = ' ';
-                remove_whitespace(argvbuf);
+                /*Replace the expression with whitespace*/
+                memset(&exprbuf[exprstart], ' ', strlen(curexpr));
+                /*Print the result of the evaluated expression into the expression buffer, store the index of the null terminator*/
+                nullidx = sprintf(&exprbuf[exprstart], "%i", res);
+                /*Replace null terminator with whitespace*/
+                exprbuf[exprstart + nullidx] = ' ';
+                remove_whitespace(exprbuf);
                 memset(curexpr, 0, 32);
+                has_run_once = 1;
         }
-        printf("%s = %s\n", original_expr, argvbuf);
+        if (!has_run_once) {
+                printf("Anna laskutehtävä komentoriviargumentteina, esim: ./main 2\\*8+10 (huom. \\*)\n");
+        } else {
+                printf("%s = %s\n", original_expr, exprbuf);
+        }
         return 0;
 }
 
@@ -57,41 +68,49 @@ int parse_expr(const char *expr, int *a, char *e, int *b)
 {
         int i = 0;
         *a = atoi(expr);
-        while (isdigit(expr[i])) {++i;}
+        while (isdigit(expr[i])) {
+                ++i;
+        }
         *e = expr[i];
         *b = atoi(&expr[i+1]);
         return 0;
 }
 
-int scan_next_expr(const char *buf, char *expr)
+int search_highest_expr(const char *buf, char *expr)
 {
-        int i = 0, i_op = 0;
-        char op = 0;
+        int char_index = 0, op_index = 0;
+        char current_op = 0;
         while (1) {
-                while ((op = buf[i]) != 0) {
+                /*Search the expression for operators*/
+                while ((current_op = buf[char_index]) != 0) {
                         int start = 0, end = 0;
-                        if (op == OPS[i_op] || op == OPS[i_op+1]) {
+                        if (current_op == OPERATORS[op_index] || current_op == OPERATORS[op_index+1]) {
                                 int temp = 0, k = 0;
-                                start = i;
-                                end = i;
+                                start = char_index;
+                                end = char_index;
+                                /*Find the beginning of the expression*/
                                 while (isdigit(buf[start-1]))
-                                        start--;
+                                        --start;
                                 temp = start;
+                                /*Find the end*/
                                 while (isdigit(buf[end+1]))
-                                        end++;
+                                        ++end;
+                                /*Copy expression from buf to expr*/
                                 while (temp <= end) {
                                         expr[k] = buf[temp];
                                         ++k;
                                         ++temp;
                                 }
-                                return start;
+                                /*Return -1 if operator is surrounded by non-digits*/
+                                return (start != char_index && end != char_index) ? start : -1;
 
                         }            
-                        ++i;
+                        ++char_index;
                 }
-                i = 0;
-                i_op += 2;
-                if (i_op > 5)
+                /*Reset char_index to the beginning of the expression, move op_index to next set of operators*/
+                char_index = 0;
+                op_index += 2;
+                if (op_index > 5)
                         break;
         }
         return -1;
